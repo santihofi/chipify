8e3ef835f4683589277581c93aea4bdd545efb7e
Santiago Hofwimmer BSc <santiago.hofwimmer2@gmail.com>
2026-05-07 23:37:01 +0200

    solve problem with installing

---
+++ check_imports.py
@@ -0,0 +1,2 @@
+import numpy, pandas, asteval
+print("numpy", numpy.__version__, "pandas", pandas.__version__, "asteval ok")
--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="chipify",
-    version="0.1.0",
+    version="0.2.0",
     description="High-Performance Mismatch Simulation Wrapper",
     author="Santiago Hofwimmer",
     packages=find_packages(), 
